8eef35fd4fd81adcc788a8fd18e0fde980b6637b
Diyorbek <baxshullayevd4@gmail.com>
2022-07-07 11:15:16 +0500

    7

--- file07.py
+++ file07.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         int: return answer
     """
-    
+    a=0
+    i=0
+    while i<len(data):
+        if data[i].isdigit():
+            a += int(data[i])
+        i+=1
+        
+    return a
+
+f=open('txt_file/data07.txt')
+data=f.read()
+print(main(data))
 # Read data from file
