9e693c4e71a0e7a352b6b40fbe21ad54ba9722c3
Diyorbek <baxshullayevd4@gmail.com>
2022-07-07 09:31:25 +0500

    3

--- file03.py
+++ file03.py
@@ -6,5 +6,16 @@ def main(data:str):
     Returns:
         list: return answer
     """
-    
-# Read data from file
+    a=[]
+    i=0
+    while i<len(data):
+        if data[i].isdigit():
+            a.append(data[i])
+
+        i+=1
+
+    return a
+
+f=open('txt_file/data03.txt')
+data=f.read()
+print(main(data))
