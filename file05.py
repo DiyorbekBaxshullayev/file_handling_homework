bbd906d4db9f9dd8b74f0633f9f1dabd3350ce8f
Diyorbek <baxshullayevd4@gmail.com>
2022-07-07 10:15:39 +0500

    5

--- file05.py
+++ file05.py
@@ -6,5 +6,23 @@ def main(data:str):
     Returns:
         list: return answer
     """
+    a=0
+    x=0
+    s=[]
+    i=0
+    while i<len(data):
+        if data[i].isdigit():
+            x+=1
+        else:
+            a+=1
+        i+=1
+    s.append(x)
+    s.append(a)
+    return s
+
+f=open('txt_file/data05.txt')
+data=f.read()
+print(main(data))
+
     
 # Read data from file
