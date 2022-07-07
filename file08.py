436ea3dc2154642a924d018e5162e326fc3cd27c
Diyorbek <baxshullayevd4@gmail.com>
2022-07-07 11:22:17 +0500

    8

--- file08.py
+++ file08.py
@@ -6,5 +6,19 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    a=0
+    x=[]
+    i=0
+    while i<len(data):
+        if data[i].isdigit():
+            x.append(int(data[i]))
+
+        i+=1
+    
+    return max(x)
+
+f=open('txt_file/data08.txt')
+data=f.read()
+print(main(data))
 
 # Read data from file
