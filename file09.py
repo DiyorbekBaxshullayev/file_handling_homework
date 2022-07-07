7f31953cf08a5619f154835b1a3414b946127e9d
Diyorbek <baxshullayevd4@gmail.com>
2022-07-07 11:26:31 +0500

    9

--- file09.py
+++ file09.py
@@ -6,5 +6,20 @@ def main(data:str):
     Returns:
         int: return answer
     """
+    a=0
+    i=0
+    x=[]
+    while i<len(data):
+        if data[i].isdigit():
+            x.append(int(data[i]))
+
+        i+=1
+    
+    return min(x)
+
+f=open('txt_file/data09.txt')
+data=f.read()
+print(main(data))
+
 
 # Read data from file
